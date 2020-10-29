4b42f5027222fdf3031876559e7f777676b747db
exel14 <showmetheway220@gmail.com>
2020-10-29 19:05:27 +0600

    add new urls

--- online_dilivery/store/urls.py
+++ online_dilivery/store/urls.py
@@ -4,9 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('',home_page,name='home'),
+    path('register/',register,name='register'),
     path('products/',product_page,name='products'),
     path('customer/<int:pk>/',customer_page,name='customer'),
-    path('create_order/',create_order,name='create_order'),
+    path('create_order/<int:pk>/',create_order,name='create_order'),
     path('update_order/<int:pk>/',update_order,name='update_order'),
-    path('delete_order/<int:pk>/',delete_order,name='delete_order')
+    path('delete_order/<int:pk>/',delete_order,name='delete_order'),
 ]
