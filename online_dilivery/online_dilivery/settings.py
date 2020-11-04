c6f7b7e6399433afda9be60e2df47f2c17f93bcf
exel14 <Showmetheway220@gmail.com>
2020-11-04 19:30:43 +0600

    add new settings

--- online_dilivery/online_dilivery/settings.py
+++ online_dilivery/online_dilivery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n(qrdvy0-g%%a3ywwf*ut04kv-god2m$s=k1_dohm%3sx6hv=8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
