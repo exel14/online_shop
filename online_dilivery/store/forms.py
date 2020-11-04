30b80fdb30aefefa1773ec34926c6a58d8b5dea3
exel14 <Showmetheway220@gmail.com>
2020-11-04 19:31:32 +0600

    add new forms

--- online_dilivery/store/forms.py
+++ online_dilivery/store/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
-from .models import Order
+from .models import Order,Customer
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+class UserProfile(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user',]
 
 class OrderForm(ModelForm):
 
